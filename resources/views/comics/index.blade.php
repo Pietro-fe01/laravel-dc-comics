8c718af0ce248708e10c5977692462244f4d4c0c
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-19 16:41:12 +0100

    Stampati nella pagina index.blade.php tutti i dati dei comics che ho inserito nel DB tramite il seeder

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.scheme');
+
+@section('page-title')
+    Homepage
+@endsection
+
+@section('main-content')
+    <section class="container">
+        <h1>Lista Comics</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Thumb</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Sale_date</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr class="table-row">
+                        <th scope="row">{{ $comic->id }}</th>
+                        <td>{{ $comic->title }}</td>
+                        <td class="w-25">{{ $comic->thumb }}</td>
+                        <td>{{ $comic->price }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>{{ $comic->sale_date }}</td>
+                        <td>{{ $comic->type }}</td>
+                        <td>{{ $comic->description }}</td>
+                        <td>...</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
+@endsection
