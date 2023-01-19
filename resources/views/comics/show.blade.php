a52abb251df88845c79786568d5c9a52e1a8c833
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-19 17:06:04 +0100

    Stampati tutti i dati del fumetto nella pagina comics.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,7 +7,26 @@
 @section('main-content')
     <section class="container">
         <h1>{{ $comic->title }}</h1>
-
-
+        <div>
+            <img src="{{ $comic->thumb }}" alt="">
+        </div>    
+        <div>
+            {{ $comic->price }} 
+        </div>    
+        <div>
+            {{ $comic->series }} 
+        </div>    
+        <div>
+            {{ $comic->sale_date }} 
+        </div>    
+        <div>
+            {{ $comic->type }}
+        </div>
+        <div>
+        {{ $comic->description }} 
+        </div>
+        <div>
+            <a href="{{ route('comics.index') }}">Torna alla pagina principale</a>
+        </div>
     </section>
 @endsection
