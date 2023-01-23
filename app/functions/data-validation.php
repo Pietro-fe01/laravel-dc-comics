a8ef1254bb4ada1832eeeffbfe12538b5fe89caf
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-23 17:54:21 +0100

    aggiunti attributi di validation

--- app/functions/data-validation.php
+++ app/functions/data-validation.php
@@ -5,7 +5,7 @@
             'thumb' => 'nullable|string',
             'price' => 'required|numeric|between:0,9999.99',
             'series' => 'required|string|max:50',
-            'sale_date' => 'required|date',
+            'sale_date' => 'required|date|after_or_equal:01/01/1930',
             'type' => 'required|string|max:40',
             'description' => 'nullable|string'
         ]);
