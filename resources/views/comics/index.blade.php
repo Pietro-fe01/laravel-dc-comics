34bb5b4b94f8c0500fb64f4b8e5db0516bd180f5
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-24 14:26:50 +0100

    Modal aggiunto style tramite classi boostrap

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -63,11 +63,11 @@
                                 <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Deleting {{ $comic->title }}</h1>
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Deleting {{ $comic->title }} with ID number {{ $comic->id }} ...</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
-                                        You will not be able to retrieve it anymore, are you sure?
+                                        You will not be able to retrieve it anymore, <strong class="text-danger text-decoration-underline">are you sure?</strong>
                                     </div>
                                     <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
