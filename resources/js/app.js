897b688f594e93d1c420a79c66647ce5400aa273
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-24 14:23:56 +0100

    Refactoring codice: - Gestita eliminazione del record senza logica JS - Trasferita logica direttamente sul model

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,16 +4,3 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ]);
-
-const modalDeleteBtn = document.querySelectorAll('.modal-delete-btn');
-const modalForm = document.querySelector('.modal-form');
-const whichComic = document.getElementById('which-one');
-
-modalDeleteBtn.forEach(elm => { 
-    elm.addEventListener('click', function(){
-        modalForm.action = "";
-        const id = this.getAttribute('data-id');
-        modalForm.action += `/${id}`;
-        whichComic.innerHTML = `record with id number ${id}.`;
-    })
-});
